19d76fd06c3c260179b700748400d56db99271d5
Lutti <lutti.t.amaral@gmail.com>
2022-04-06 12:16:22 -0700

    javascript silly exercises =3

---
+++ fundamentals/04.01-javascript/Exercise3/rectangle.js
@@ -0,0 +1,8 @@
+// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+// Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = base + base + height + height;
