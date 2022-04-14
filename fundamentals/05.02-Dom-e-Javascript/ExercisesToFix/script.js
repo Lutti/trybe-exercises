ce4762c3ba05e1448dbbdbcfb38ff4222a03f2cd
Lutti <lutti.t.amaral@gmail.com>
2022-04-14 11:48:18 -0700

    Exercises to fix lesson content till part 2 done

---
+++ fundamentals/05.02-Dom-e-Javascript/ExercisesToFix/script.js
@@ -0,0 +1,52 @@
+// 1
+let elem = document.querySelector("#elementoOndeVoceEsta");
+
+// 2
+elem.parentElement.style.backgroundColor = 'red';
+
+// 3
+//Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
+let primFilDoFil = document.querySelector("#primeiroFilhoDoFilho").innerHTML = 'Texto para primeiro filho do filho';
+
+// 4 
+//Acesse o primeiroFilho a partir de pai
+let primFil = document.querySelector('pai').firstChild;
+
+// 5
+//Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta
+let primFil2 = document.querySelector('#elementoOndeVoceEsta').parentElement.firstChild;
+
+// 6
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta
+let att = document.querySelector('#elementoOndeVoceEsta').nextSibling;
+
+// 7
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta
+let sib = document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+
+// 8
+// Agora acesse o terceiroFilho a partir de pai
+let terFil = document.querySelector('#pai').children[2];
+
+//PART 2
+// 1
+// Crie um irmão para elementoOndeVoceEsta .
+let loc = document.getElementById('pai');
+let irm = document.createElement('section');
+loc.appendChild(irm);
+
+// 2
+// Crie um filho para elementoOndeVoceEsta .
+let loc2 = document.getElementById('elementoOndeVoceEsta');
+let fil = document.createElement('section');
+loc2.appendChild(fil);
+
+// 3
+// Crie um filho para primeiroFilhoDoFilho 
+let loc3 = document.getElementById('primeiroFilhoDoFilho');
+let fil3 = document.createElement('section');
+loc3.appendChild(fil3);
+
+// 4
+// A partir desse filho criado, acesse terceiroFilho .
+let terceiroFil = fil3.children[2];
