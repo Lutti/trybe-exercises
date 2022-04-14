74b94ea88d4fb8fa4a8bf16495c8b5af1df35b85
Lutti <lutti.t.amaral@gmail.com>
2022-04-14 12:16:49 -0700

    Hashtag missing bugged the previus code - FIXED

--- fundamentals/05.02-Dom-e-Javascript/ExercisesToFix/script.js
+++ fundamentals/05.02-Dom-e-Javascript/ExercisesToFix/script.js
@@ -10,7 +10,7 @@ let primFilDoFil = document.querySelector("#primeiroFilhoDoFilho").innerHTML = '
 
 // 4 
 //Acesse o primeiroFilho a partir de pai
-let primFil = document.querySelector('pai').firstChild;
+let primFil = document.querySelector('#pai').firstChild;
 
 // 5
 //Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta
@@ -31,7 +31,7 @@ let terFil = document.querySelector('#pai').children[2];
 //PART 2
 // 1
 // Crie um irmão para elementoOndeVoceEsta .
-let loc = document.getElementById('pai');
+let loc = document.querySelector('#pai');
 let irm = document.createElement('section');
 loc.appendChild(irm);
 
@@ -45,8 +45,10 @@ loc2.appendChild(fil);
 // Crie um filho para primeiroFilhoDoFilho 
 let loc3 = document.getElementById('primeiroFilhoDoFilho');
 let fil3 = document.createElement('section');
+fil3.id = 'part2-ex3';
 loc3.appendChild(fil3);
 
 // 4
 // A partir desse filho criado, acesse terceiroFilho .
-let terceiroFil = fil3.children[2];
+let terceiroFil = fil3.parentElement.parentElement.parentElement.children[2];
+//console.log(terceiroFil);
